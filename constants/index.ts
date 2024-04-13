d6338a8fab595b0e67ef5969fbb265f1ab8c854d
Sanskar-12 <sanskarv2004@gmail.com>
2024-04-13 06:57:50 +0530

    frontend

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/Video.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/room.svg",
+  },
+];
+
+export default sidebarLinks;
